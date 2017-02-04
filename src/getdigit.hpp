8ef791ac7a608bc621c53cb2652dc5ffb649af88
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-04 16:35:40 +0000

    .

--- src/getdigit.hpp
+++ src/getdigit.hpp
@@ -2,16 +2,10 @@
 # define __getdigit__hpp
 # include <boost/cstdint.hpp>
 # include <intlen.hpp>
+# include <eint_t.hpp>
 # include <cmath>
 namespace mdl
 {
-    # ifdef ARC64
-        typedef boost::uint64_t uint_t;
-    # elif ARC32
-        typedef boost::uint32_t uint_t;
-    # else
-        typedef unsigned int uint_t;
-    # endif
     boost::uint8_t getdigit(uint_t __uint, std::size_t __unit);  
 }
 
